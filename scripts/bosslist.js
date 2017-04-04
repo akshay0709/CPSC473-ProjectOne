43cb13ce9812040a47c6490f01e58a884aa2ff09
Akshay Pawar <akshay070993@gmail.com>
2017-04-03 18:54:57 -0700

    some css changes
    some css changes

--- scripts/bosslist.js
+++ scripts/bosslist.js
@@ -17,7 +17,8 @@
               allComments = s;
               allComments.forEach(function(item){
                 //console.log("this is item " + item.id);
-                $("#comments").prepend("<div><h5>" + item.bossName + " - " + item.bossDepartment + " - Rating:" + item.rating + "</h5>" + item.bossFeedback + "</div>");
+                //$("#comments").prepend("<div><h5>" + item.bossName + " - " + item.bossDepartment + " - Rating:" + item.rating + "</h5>" + item.bossFeedback + "</div>");
+                $("#comments").prepend("<div class='bg-success feedbackdata'><h3>Boss Name: <i><b>" + item.bossName + "</b></i></h3><h4>Rating :<i><b>" + item.rating + "</b></i></h4><h4>Department: <i><b>" + item.bossDepartment +  "</b></i></h4><p><i><b>" + item.bossFeedback +  "</b></i></p></div>");
               });
           });
     };
